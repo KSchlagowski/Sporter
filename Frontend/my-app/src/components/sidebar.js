b080942c2d26bf73f736e7927139e952ba470da2
Kamil-PLewka <kamilplewka12a@wp.pl>
2021-04-14 23:38:56 +0200

    Work In Progress with MainPage

---
+++ Frontend/my-app/src/components/sidebar.js
@@ -0,0 +1,122 @@
+import { Navbar, Nav, Image } from "react-bootstrap";
+import "../styles/sideBar.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ArrowRight from "../components/arrowRight";
+import image from "../photos/shoes.jpg";
+import image2 from "../photos/shoes2.jpg";
+import image3 from "../photos/shoes3.jpg";
+import { useState } from "react";
+export default function SideBar() {
+  const [currentPhoto, setcurrentPhoto] = useState(0);
+  let opacity = 0;
+  function ReduceOpacity() {
+    setTimeout(() => {
+      const opacityReducer = setInterval(() => {
+        opacity = opacity - 0.005;
+        document.querySelector("#photos").style.opacity = opacity;
+        if (opacity < 0.001) {
+          clearInterval(opacityReducer);
+          InreaseOpacity();
+          document.querySelector("#photos").src=image2
+        }
+      }, 7);
+    }, 6000);
+  }
+  function InreaseOpacity() {
+    const opacityIncrease = setInterval(() => {
+      opacity = opacity + 0.005;
+      document.querySelector("#photos").style.opacity = opacity;
+      if (opacity > 1) {
+        clearInterval(opacityIncrease);
+        ReduceOpacity();
+      }
+    }, 7);
+  }
+
+InreaseOpacity()
+  return (
+    <>
+      <main>
+        <Navbar
+          style={{ width: "300px", maxHeight: "650px" }}
+          bg="dark"
+          variant="dark"
+        >
+          <Nav
+            style={{ width: "150px", paddingLeft: "20px", display: "flex" }}
+            className=" sideBar d-flex flex-column"
+          >
+            <Nav.Link className="category" href="#features">
+              Football <ArrowRight />
+            </Nav.Link>
+            <Nav.Link className="category" href="#pricing">
+              Basketball
+              <ArrowRight />
+            </Nav.Link>
+            <Nav.Link className="category" href="#home">
+              Fitness
+              <ArrowRight />
+            </Nav.Link>
+
+            <Nav.Link className="category" href="#features">
+              Football <ArrowRight />
+            </Nav.Link>
+            <Nav.Link className="category" href="#pricing">
+              Basketball
+              <ArrowRight />
+            </Nav.Link>
+            <Nav.Link className="category" href="#home">
+              Fitness
+              <ArrowRight />
+            </Nav.Link>
+
+            <Nav.Link className="category" href="#features">
+              Football <ArrowRight />
+            </Nav.Link>
+            <Nav.Link className="category" href="#pricing">
+              Basketball
+              <ArrowRight />
+            </Nav.Link>
+            <Nav.Link className="category" href="#home">
+              Fitness
+              <ArrowRight />
+            </Nav.Link>
+
+            <Nav.Link className="category" href="#features">
+              Football <ArrowRight />
+            </Nav.Link>
+            <Nav.Link className="category" href="#pricing">
+              Basketball
+              <ArrowRight />
+            </Nav.Link>
+            <Nav.Link className="category" href="#home">
+              Fitness
+              <ArrowRight />
+            </Nav.Link>
+
+            <Nav.Link className="category" href="#features">
+              Football <ArrowRight />{" "}
+            </Nav.Link>
+            <Nav.Link className="category" href="#pricing">
+              Basketball
+              <ArrowRight />{" "}
+            </Nav.Link>
+            <Nav.Link className="category" href="#home">
+              Fitness
+              <ArrowRight />{" "}
+            </Nav.Link>
+          </Nav>
+        </Navbar>
+
+        <figure className="photos">
+          <img
+            id="photos"
+            src={image}
+            className="img-fluid"
+            alt="Responsive image"
+          ></img>
+        </figure>
+      </main>
+    </>
+  );
+}
--- Frontend/my-app/src/views/MainPage.js
+++ Frontend/my-app/src/views/MainPage.js
@@ -1,6 +1,8 @@
 import NavBar from "../components/navbar"
+import SideBar from "../components/sidebar"
 export default function MainPage() {
   return <>
     <NavBar/>
+    <SideBar/>
   </>;
 }
