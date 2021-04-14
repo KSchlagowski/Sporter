dcd2b9578e4bef42bbcb88f079888e9733bda6eb
KSchlagowski <mediewilnp@gmail.com>
2021-04-14 15:10:17 +0200

    Update ItemModel

--- Backend/Sporter.Core/Models/ItemModel.cs
+++ Backend/Sporter.Core/Models/ItemModel.cs
@@ -5,6 +5,7 @@ namespace Sporter.Core.Models
         public string Name { get; set; }
         public string Category { get; set; }
         public decimal Price { get; set; }
+        public string Location { get; set; }
         public Guid OwnerId { get; set; }
         public DateTime PublishDate { get; set; }
     }
