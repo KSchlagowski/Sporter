ae2b4082811c1517bec5a0be27196f1b02a01e7b
KSchlagowski <mediewilnp@gmail.com>
2021-05-18 19:09:14 +0200

    Update models for database

--- Backend/Sporter.Domain/Models/ItemModel.cs
+++ Backend/Sporter.Domain/Models/ItemModel.cs
@@ -4,11 +4,16 @@ namespace Sporter.Domain.Models
 {
     public class ItemModel
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Category { get; set; }
         public decimal Price { get; set; }
-        public string Location { get; set; }
-        public Guid OwnerId { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
         public DateTime PublishDate { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public int BuyerId { get; set; }
+
+        public virtual AuctionModel Auction { get; set; }
     }
 }
