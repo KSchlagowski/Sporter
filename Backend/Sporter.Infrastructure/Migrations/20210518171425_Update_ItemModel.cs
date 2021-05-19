ed5df92c207be857800892beec89605b3eb47d18
KSchlagowski <mediewilnp@gmail.com>
2021-05-19 09:38:56 +0200

    Update migrations

---
+++ Backend/Sporter.Infrastructure/Migrations/20210518171425_Update_ItemModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sporter.Infrastructure.Migrations
+{
+    public partial class Update_ItemModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAvailable",
+                table: "ItemModels",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAvailable",
+                table: "ItemModels");
+        }
+    }
+}
