a417616297d263bc9afa4de919859e698ba0ea26
KSchlagowski <mediewilnp@gmail.com>
2021-06-02 00:39:37 +0200

    Make validation for new clients and items

---
+++ Backend/Sporter.Application/Validators/ViewModels/Client/NewAddressValidator.cs
@@ -0,0 +1,19 @@
+using FluentValidation;
+using Sporter.Application.ViewModels.Client;
+
+namespace Sporter.Application.Validators.ViewModels.Client
+{
+    public class NewAddressValidator : AbstractValidator<NewAddressVm>
+    {
+        public NewAddressValidator()
+        {
+            RuleFor(x => x.Id).NotNull();
+            RuleFor(x => x.Street).NotEmpty().MinimumLength(3).MaximumLength(50);
+            RuleFor(x => x.BuildingNumber).NotEmpty().MinimumLength(1).MaximumLength(10);
+            RuleFor(x => x.FlatNumber).NotEmpty().MinimumLength(1).MaximumLength(10);
+            RuleFor(x => x.ZipCode).NotEmpty().MinimumLength(3).MaximumLength(10);
+            RuleFor(x => x.City).NotEmpty().MinimumLength(3).MaximumLength(50);
+            RuleFor(x => x.Country).NotEmpty().MinimumLength(3).MaximumLength(50);
+        }
+    }
+}
