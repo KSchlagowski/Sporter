4aa151e594f34a161e3250a43df511c59274cc77
Krzysztof Białk <krzys.bialk1@gmail.com>
2021-12-09 12:53:50 +0100

    node frontend

---
+++ Frontend/newFront/app.js
@@ -0,0 +1,18 @@
+const express = require('express'),
+    PORT = process.env.PORT || 3000,
+    app = express(),
+    ejsMate = require('ejs-mate'),
+    dotenv = require('dotenv'),
+    path = require('path'),
+    cors = require('cors');
+
+dotenv.config({path:'./config/config.env'});
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use(cors());
+app.use((req, res, next) => {next()});
+app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}`));
